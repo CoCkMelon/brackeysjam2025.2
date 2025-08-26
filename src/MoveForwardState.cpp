39b5e0a23ce2e3d4f99b4c11601e119189fe21da
joshuawereley <joshuawereley@icloud.com>
2025-08-26 14:41:12 +0200

    Add constructor and remove paramenters from rest of functions.

--- src/MoveForwardState.cpp
+++ src/MoveForwardState.cpp
@@ -1,24 +1,28 @@
 #include "MoveForwardState.h"
 
-void MoveForwardState::moveCarForward(Car* car) override {
+MoveForwardState::MoveForwardState(Car* car) {
+  this->car = car;
+}
+
+void MoveForwardState::moveCarForward() override {
   std::cout << "Already moving forward.\n";
 }
 
-void MoveForwardState::moveCarBackward(Car* car) override {
+void MoveForwardState::moveCarBackward() override {
   std::cout << "Switching from moving forward to backward.\n";
   car->setCurrentState(car->getMoveBackward());
 }
 
-void MoveForwardState::makeCarJump(Car* car) override {
+void MoveForwardState::makeCarJump() override {
   std::cout << "Jumping while moving forward.\n";
   car->setCurrentState(car->getJumpUp());
 }
 
-void MoveForwardState::switchOnBoost(Car* car) override {
+void MoveForwardState::switchOnBoost() override {
   std::cout << "Activating boost while moving forward.\n";
   car->setCurrentState(car->getBoostOn());
 }
 
-void MoveForwardState::swtichOffBoost(Car* car) override {
+void MoveForwardState::swtichOffBoost() override {
   std::cout << "Boost already off while moving forward.\n";
 }
