1de6892a3e70af9c4c8b907ef04f3b60dc4e5958
joshuawereley <joshuawereley@icloud.com>
2025-08-26 14:38:33 +0200

    Code functions for MoveBackwardState

--- src/MoveBackwardState.cpp
+++ src/MoveBackwardState.cpp
@@ -1,4 +1,27 @@
 #include "MoveBackwardState.h"
 
+MoveBackwardState::MoveBackwardState(Car* car) {
+  this->car = car;
+}
+
 void MoveBackwardState::moveCarForward() override {
-  std::cout << "Switching from moving backward to moving
+  std::cout << "Switching from moving backward to forward.\n";
+  car->setCurrentState(car->getMoveForward());
+}
+
+void MoveBackwardState::moveCarBackward() override {
+  std::cout << "Already moving backward.\n";
+}
+
+void MoveBackwardState::makeCarJump() override {
+  std::cout << "Jumping while moving backward.\n";
+  car->setCurrentState(car->getJumpUp());
+}
+
+void MoveBackwardState::switchOnBoost() override {
+  std::cout << "Cannot boost while moving backward.\n";
+}
+
+void MoveBackwardState::switchOffBoost() override {
+  std::cout << "Boost already off while moving backward.\n";
+}
