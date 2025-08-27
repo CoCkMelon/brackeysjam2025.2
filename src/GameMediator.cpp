a20f482464e8e25bb26b8099da9f9a9f4a0abecf
joshuawereley <joshuawereley@icloud.com>
2025-08-27 17:33:48 +0200

    Add function code for GameMediator.cpp

---
+++ src/GameMediator.cpp
@@ -0,0 +1,13 @@
+#include "GameMediator.h"
+
+GameMediator::GameMediator(Car* car) {
+  this->car = car;
+}
+
+void GameMediator::notify(std::string event) override {
+  if (event == "CarDamaged") {
+    std::cout << "[Mediator] Car was hit! Updating UI.\n";
+  }
+}
+
+
